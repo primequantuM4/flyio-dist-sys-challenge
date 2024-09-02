b4b97da83ba5f8b0cad9b876ced1629702383cfc
primequantuM4 <gediezra@gmail.com>
2024-09-02 12:48:18 +0300

    fix: test shell to execute the actual command

--- challenge-2/test.sh
+++ challenge-2/test.sh
@@ -5,5 +5,5 @@ go install .
 cd ~/maelstrom
 
 ./maelstrom test -w unique-ids --bin ~/go/bin/maelstrom-unique-ids \
-    --time-limit 30 --rate 1000 --node-count 3 \
-    --availability total --nemesis partition
+--time-limit 30 --rate 1000 --node-count 3 \
+--availability total --nemesis partition
